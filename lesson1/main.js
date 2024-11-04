beb6157192292f588d72605633a72b4cf5eb2ec8
GrayHead <ss.zhuravlov@gmail.com>
2024-11-04 20:01:07 +0200

    lesson 1

---
+++ lesson1/main.js
@@ -0,0 +1,17 @@
+// let a;
+// let a = 'asgdahsg';
+// a = 127342154;
+
+// let a = 'asdsad',
+//     b = 12321;
+//
+// let arr = [11, 22, 'adsasd', 33];
+
+console.log('start');
+let answer = prompt('скікі тобі рочків?');
+console.log('end');
+console.log(answer);
+
+console.log('5' === 5); // '5' == '5' -> 01010010101 == 01010010101
+console.log('5' === 5); // false
+// > < >= <= === !== == !=
